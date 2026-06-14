e6611cdcc5c6813e35be3f8564a0def955623cb6
TioSEUS <sarvitor13@gmail.com>
2026-06-14 05:25:40 -0400

    Update 02-drivers.sh

--- modules/02-drivers.sh
+++ modules/02-drivers.sh
@@ -1,1 +1,32 @@
 
+#!/usr/bin/env bash
+
+set -e
+
+echo "Detectando hardware..."
+
+if lspci | grep -qi nvidia; then
+
+    sudo pacman -S --needed --noconfirm \
+    nvidia \
+    nvidia-utils \
+    nvidia-settings
+
+fi
+
+if lspci | grep -qi amd; then
+
+    sudo pacman -S --needed --noconfirm \
+    mesa \
+    vulkan-radeon \
+    lib32-vulkan-radeon
+
+fi
+
+if lspci | grep -qi intel; then
+
+    sudo pacman -S --needed --noconfirm \
+    mesa \
+    vulkan-intel
+
+fi
