0aaee7a926ef9ba6e090c1125a86af47546a197c
TioSEUS <sarvitor13@gmail.com>
2026-06-14 05:01:14 -0400

    Delete modules directory

--- modules/01-prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "-----------------------------------------------------------------"
-echo "--> MÓDULO 1: Preparação do Sistema"
-echo "-----------------------------------------------------------------"
-
-sudo pacman -Sy --noconfirm archlinux-keyring
-sudo pacman -S --noconfirm --needed reflector
-sudo reflector --latest 12 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -Syy
--- modules/04-themes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "-----------------------------------------------------------------"
-echo "--> MÓDULO 4: Temas"
-echo "-----------------------------------------------------------------"
-
-paru -S --noconfirm --needed catppuccin-gtk-theme-mocha papirus-icon-theme || true
