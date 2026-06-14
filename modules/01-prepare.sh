ba539c97fca83fff47e9af2335f0ab79c9fbb7ad
TioSEUS <sarvitor13@gmail.com>
2026-06-14 03:48:27 -0400

    Update 01-prepare.sh

--- modules/01-prepare.sh
+++ modules/01-prepare.sh
@@ -3,12 +3,7 @@ echo "-----------------------------------------------------------------"
 echo "--> MÓDULO 1: Preparação do Sistema"
 echo "-----------------------------------------------------------------"
 
-echo "--> Atualizando chaves..."
 sudo pacman -Sy --noconfirm archlinux-keyring
-
-echo "--> Otimizando mirrors..."
 sudo pacman -S --noconfirm --needed reflector
 sudo reflector --latest 12 --sort rate --save /etc/pacman.d/mirrorlist
-
-echo "--> Sincronizando repositórios..."
 sudo pacman -Syy
