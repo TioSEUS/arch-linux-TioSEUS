b8ff7bb091644e8c78e92a1b454f815bdcd2eea4
TioSEUS <sarvitor13@gmail.com>
2026-06-14 05:28:15 -0400

    Update 05-services.sh

--- modules/05-services.sh
+++ modules/05-services.sh
@@ -1,1 +1,16 @@
 
+#!/usr/bin/env bash
+
+set -e
+
+sudo systemctl enable NetworkManager
+sudo systemctl enable bluetooth
+sudo systemctl enable sddm
+
+sudo systemctl start NetworkManager
+sudo systemctl start bluetooth
+
+flatpak remote-add \
+--if-not-exists \
+flathub \
+https://dl.flathub.org/repo/flathub.flatpakrepo
