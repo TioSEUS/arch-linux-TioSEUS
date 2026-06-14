59574559ac0412b6191f6a55342bb201df4fc062
TioSEUS <sarvitor13@gmail.com>
2026-06-14 05:24:50 -0400

    Update 01-system.sh

--- modules/01-system.sh
+++ modules/01-system.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
 
+set -e
+
+echo "Atualizando keyring..."
+
+sudo pacman -Sy --noconfirm archlinux-keyring
+
+echo "Atualizando bancos..."
+
+sudo pacman -Syy --noconfirm
+
+echo "Atualizando sistema..."
+
+sudo pacman -Syu --noconfirm
