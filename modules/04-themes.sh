e81d7ca3ac9c8ff0a37e67b6c432e27677e19d36
TioSEUS <sarvitor13@gmail.com>
2026-06-14 03:28:30 -0400

    Create 04-themes.sh

---
+++ modules/04-themes.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "-----------------------------------------------------------------"
+echo "--> MÓDULO 4: Temas e Aparência"
+echo "-----------------------------------------------------------------"
+
+paru -S --noconfirm --needed \
+    catppuccin-gtk-theme-mocha catppuccin-icon-theme papirus-icon-theme || true
+
+echo "--> Temas instalados!"
