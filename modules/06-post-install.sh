c6c12b80335b37445be95a0418ad744e0ae7f0ed
TioSEUS <sarvitor13@gmail.com>
2026-06-14 03:29:41 -0400

    Create 06-post-install.sh

---
+++ modules/06-post-install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "-----------------------------------------------------------------"
+echo "--> MÓDULO 6: Pós-instalação"
+echo "-----------------------------------------------------------------"
+
+fc-cache -fv
+xdg-user-dirs-update
+
+echo "✅ Configurações finais aplicadas!"
