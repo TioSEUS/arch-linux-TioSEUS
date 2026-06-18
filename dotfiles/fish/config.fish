0613bc6af724b7ec91525b0816291cfa772796da
TioSEUS <sarvitor13@gmail.com>
2026-06-18 10:01:44 -0400

    Update config.fish

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,26 +1,26 @@
 # Iniciar o Hyprland automaticamente ao logar no TTY1
 if status is-login
-        if test (tty) = "/dev/tty1"
-        exec start-Hyprland
-      end
+    if test (tty) = "/dev/tty1"
+        exec Hyprland
+    end
 end
 
 if status is-interactive
-        #Comandos para rodar em sessões interativas
+    # Comandos para rodar em sessões interativas
     fastfetch
 
-        #Desativar a mensagem de Boas-Vindas do fish
-        set fish_greeting ''
-        if type -q starship
+    # Desativar a mensagem de Boas-Vindas do fish
+    set -g fish_greeting ''
 
-        #Inicializar o prompt do Starship
+    if type -q starship
+        # Inicializar o prompt do Starship
         starship init fish | source
-     end
+    end
 end
 
 
 # --- Aliases (Atalhos de Comandos) ---
-#Atalhos para facilitar a vida no Arch Seu Inutio
+# Atalhos para facilitar a vida no Arch Seu Inutio
 alias fm="spf"
 alias atualizar='yay -Syu'
 alias limpar='yay -Sc'
@@ -29,8 +29,4 @@ alias conf-fish='nano ~/.config/fish/config.fish'
 alias conf-kitty='nano ~/.config/kitty/kitty.conf'
 
 # --- Otimização para Jogos (AMD) ---
-set -gx RADV_PERFTEST aco
 set -gx AMD_DEBUG ngg
-set -gx GTK_IM_MODULE xim
-set -gx QT_IM_MODULE xim
-set -g fish_greeting
