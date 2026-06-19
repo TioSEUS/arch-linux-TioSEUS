955f61825c87fb2be0bc2aa0699d958abfd5b0ba
TioSEUS <sarvitor13@gmail.com>
2026-06-18 22:55:45 -0400

    Create powermenu.sh

---
+++ dotfiles/rofi/powermenu/type-4/powermenu.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+## TioSEUS Power Menu
+## Mostra 5 botões: lock, suspend, logout, reboot, shutdown
+## Adaptado de: adi1090x/rofi
+
+# Current Theme
+dir="$HOME/.config/rofi/powermenu/type-4"
+theme='style-5'
+
+# CMDs
+uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
+
+# Options (ícones feather)
+shutdown=''
+hibernate=''
+reboot=''
+lock=''
+suspend=''
+logout=''
+yes=''
+no=''
+
+# Rofi CMD
+rofi_cmd() {
+    rofi -dmenu \
+        -p "Goodbye ${USER}" \
+        -mesg "Uptime: $uptime" \
+        -theme ${dir}/${theme}.rasi
+}
+
+# Confirmation CMD
+confirm_cmd() {
+    rofi -dmenu \
+        -p 'Confirmation' \
+        -mesg 'Are you Sure?' \
+        -theme ${dir}/shared/confirm.rasi
+}
+
+# Ask for confirmation
+confirm_exit() {
+    echo -e "$yes\n$no" | confirm_cmd
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+    echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == '--suspend' ]]; then
+            amixer set Master mute 2>/dev/null
+            systemctl suspend
+        elif [[ $1 == '--logout' ]]; then
+            hyprctl dispatch exit
+        fi
+    else
+        exit 0
+    fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+        run_cmd --shutdown
+        ;;
+    $reboot)
+        run_cmd --reboot
+        ;;
+    $lock)
+        if [[ -x "$(command -v hyprlock)" ]]; then
+            hyprlock
+        elif [[ -x "$(command -v swaylock)" ]]; then
+            swaylock
+        fi
+        ;;
+    $suspend)
+        run_cmd --suspend
+        ;;
+    $logout)
+        run_cmd --logout
+        ;;
+esac
