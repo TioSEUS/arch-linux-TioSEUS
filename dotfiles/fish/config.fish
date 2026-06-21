cd840d692ade06754b731cb7b19520e489517d86
TioSEUS <sarvitor13@gmail.com>
2026-06-21 00:50:59 -0400

    Update config.fish

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -20,13 +20,25 @@ end
 
 
 # --- Aliases (Atalhos de Comandos) ---
-# Atalhos para facilitar a vida no Arch Seu Inutio
-alias fm="spf"
+alias fm="yazi"
 alias atualizar='yay -Syu'
 alias limpar='yay -Sc'
-alias conf-hypr='nano ~/.config/hypr/hyprland.conf'
-alias conf-fish='nano ~/.config/fish/config.fish'
-alias conf-kitty='nano ~/.config/kitty/kitty.conf'
+
+# === ATALHOS DE CONFIGURAÇÃO (abrem no VS Code) ===
+alias conf-hypr='code ~/.config/hypr/hyprland.conf'
+alias conf-fish='code ~/.config/fish/config.fish'
+alias conf-kitty='code ~/.config/kitty/kitty.conf'
+alias conf-rofi='code ~/.config/rofi/config.rasi'
+alias conf-waybar='code ~/.config/waybar/config.jsonc'
+alias conf-waybar-style='code ~/.config/waybar/style.css'
+alias conf-swaync='code ~/.config/swaync/config.json'
+alias conf-cava='code ~/.config/cava/config'
+alias conf-fastfetch='code ~/.config/fastfetch/config.jsonc'
+
+# === EDITOR PADRÃO ===
+set -gx EDITOR code
+set -gx VISUAL code
+set -gx SUDO_EDITOR 'code --wait'
 
 # --- Otimização para Jogos (AMD) ---
 set -gx AMD_DEBUG ngg
